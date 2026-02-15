912d798e33d426d07dbca151031b820e1f5987b3
mauridf <mauridf@gmail.com>
2026-02-15 13:20:00 -0300

    feat: add EF Core, PostgreSQL provider and base DbContext

--- Patrimonium.API/Program.cs
+++ Patrimonium.API/Program.cs
@@ -1,23 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Patrimonium.Infrastructure.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<PatrimoniumDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ Patrimonium.Infrastructure/Data/Context/PatrimoniumDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Patrimonium.Infrastructure.Data.Context
+{
+    public class PatrimoniumDbContext : DbContext
+    {
+        public PatrimoniumDbContext(DbContextOptions<PatrimoniumDbContext> options)
+            : base(options) { }
+
+        // DbSets (vão crescer por módulo)
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Property> Properties => Set<Property>();
+        public DbSet<Person> People => Set<Person>();
+        public DbSet<Contract> Contracts => Set<Contract>();
+        public DbSet<FinancialTransaction> FinancialTransactions => Set<FinancialTransaction>();
+        public DbSet<Maintenance> Maintenances => Set<Maintenance>();
+        public DbSet<Inspection> Inspections => Set<Inspection>();
+        public DbSet<Document> Documents => Set<Document>();
+        public DbSet<Media> Media => Set<Media>();
+        public DbSet<MonthClosing> MonthClosings => Set<MonthClosing>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PatrimoniumDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
